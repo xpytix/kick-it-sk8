650afbfb55c54517d75580063d61d664a375961d
xpytix <Pytelek99@gmail.com>
2020-10-19 15:20:21 +0200

    Updates

---
+++ precache-manifest.73595585e64d635271debfe2f5caa502.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99eec455970fc7cefe85d532134f5a9e",
+    "url": "/kick-it-sk8/index.html"
+  },
+  {
+    "revision": "fdc0c3018ce871d40e22",
+    "url": "/kick-it-sk8/static/css/main.2890e824.chunk.css"
+  },
+  {
+    "revision": "f3a90bb4a5acdbf22513",
+    "url": "/kick-it-sk8/static/js/2.e585e35c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/kick-it-sk8/static/js/2.e585e35c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc0c3018ce871d40e22",
+    "url": "/kick-it-sk8/static/js/main.d56134a2.chunk.js"
+  },
+  {
+    "revision": "bec4a5383282b9c50e01",
+    "url": "/kick-it-sk8/static/js/runtime-main.e366a0dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kick-it-sk8/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7aaa513aa58b2221c14dad393cbd03e1",
+    "url": "/kick-it-sk8/static/media/question-circle-regular.7aaa513a.svg"
+  },
+  {
+    "revision": "b6d43522021ced95ac54c1f5835253c7",
+    "url": "/kick-it-sk8/static/media/skateboard_all.b6d43522.png"
+  },
+  {
+    "revision": "0d567f84b2b2e800647138f3e503d946",
+    "url": "/kick-it-sk8/static/media/skateboard_easy.0d567f84.png"
+  },
+  {
+    "revision": "8597835bb4f07da4121d97079d5fb0e8",
+    "url": "/kick-it-sk8/static/media/skateboard_hard.8597835b.png"
+  },
+  {
+    "revision": "294c08d7781949742da8f5ec89b56e16",
+    "url": "/kick-it-sk8/static/media/skateboard_medium.294c08d7.png"
+  }
+]);
